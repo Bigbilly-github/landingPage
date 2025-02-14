e273cf45242ea984564b2d122f3c3ad877c6c8ab
Bigbilly-github <abiolatwitter97@gmail.com>
2025-02-14 16:09:08 +0100

    adding responsivenes to the hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,16 +7,16 @@ function Hero (){
 
     return(
         <>
-         <section  className="w-full h-auto flex justify-center sm:bg-[red] md:bg-[blue] lg:bg-[green] xl:bg-[orange] ">
-            <div className="w-[1200px] h-[764px]  flex items-center jus ml-[45px]">
-                <div className='w-[630px] h-[535px]'>
+         <section  className="w-full h-auto flex justify-center   ">
+            <div className="xl:w-[1200px] w-[100vw] h-[664px] flex md:flex-row items-center justify-center flex-col md:ml-[45px]">
+            <div className='w-[630px] h-[535px]   md:items-start flex  flex-col items-center md:ml-[20px] xl:ml-0'>
                         <p className='w-[410px] h-[30px] font-poppins font-bold text-[20px] tracking-normal text-[#DF6951]'>
                             BEST DESTINATIONS AROUND THE WORLD
                         </p>
-                        <h1 className='font-volx w-[477px] h-[267px] font-bold text-[64px] leading-[89px] text-[#181E4B]'>
+                        <h1 className='font-volx w-[477px] h-[267px] md:ml-0 ml-[60px] font-bold text-[64px] leading-[89px] text-[#181E4B]'>
                             Travel, enjoy and live a new and full life
                         </h1>
-                        <p className='font-poppins font-medium text-[16px] leading-[30px] w-[477px] h-[90] text-[#5E6282]'>
+                        <p className='font-poppins font-medium text-[16px] md:ml-0  ml-[50px] leading-[30px] sm:w-[477px] w-[457px] h-[90] text-[#5E6282]'>
                             Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.
                         </p>
                         <div className='flex mt-[34px] items-center'>
@@ -47,3 +47,5 @@ function Hero (){
     
 }
 export default Hero
+
+
